9371eb399396c97f74df47f4be43f920d88c3e5b
Alan Sun <56702016+ACCount-Nine38@users.noreply.github.com>
2023-09-16 03:27:01 -0400

    initial commit App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+  const [page, setPage] = useState("menu");
+
+  function GetPage() {
+      switch (page) {
+        // swap page
+      }
+  }
+
+  useEffect(() => {
+      if (game_mode !== -1) {
+          setPage('game')
+      }
+  }, [game_mode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className = "App">
+          {GetPage()}
+      </div>
   );
 }
 
